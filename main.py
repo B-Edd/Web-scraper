60a33fdceddf3f831a1b595714088362cd2025df
EddieWird <121364681+EddieWird@users.noreply.github.com>
2023-05-25 23:42:26 +0000

    h

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-appende = []
-splited = []
+l = []
 
-url = "https://en.wikipedia.org/wiki/Communication"
+url = "https://en.wikipedia.org/wiki/Communication"  # Replace with the desired website URL
 response = requests.get(url)
 html_content = response.text
 
 soup = BeautifulSoup(html_content, "html.parser")
+text_content = soup.get_text()
 
-words = html_content.split()
+# Extract words from the text content
+words = re.findall(r'\b\w+\b', text_content)
 
-
+# Print each word
 for word in words:
-  splited.append(word)
-print(splited)
+    l.append(word)
+
+for i in l:
+  print(i + " ")
